3762e942ad7d9b78927c2a146f0402d16f0c81b8
Jarle Aase <jarle@jgaa.com>
2018-10-03 09:22:40 +0300

    Updated tests.

--- tests/general_tests.cpp
+++ tests/general_tests.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #define LOGFAULT_ENABLE_ALL
+#define LOGFAULT_USE_SYSLOG
 #include "logfault/logfault.h"
 
 using namespace std;
@@ -10,7 +11,12 @@ int main( int argc, char *argv[]) {
 
     std::unique_ptr<logfault::Handler> filehandler{new logfault::StreamHandler(clog, logfault::LogLevel::TRACE)};
 
-    logfault::LogManager::Instance().AddHandler(move(filehandler));
+
+    //logfault::LogManager::Instance().AddHandler(move(filehandler));
+
+    std::unique_ptr<logfault::Handler> syslog_handler{ new logfault::SyslogHandler(logfault::LogLevel::DEBUG) };
+    logfault::LogManager::Instance().AddHandler(move(syslog_handler));
+
 
     LFLOG_DEBUG << "Testing" << 1 << 2 << 3;
     LFLOG_ERROR << "Did something fail?";
