25521ee5ab01a608a49d65c5c9ca2b2f61b8119f
Jarle Aase <jarle@jgaa.com>
2024-03-09 17:37:24 +0200

    Added conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,52 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.build import check_max_cppstd, check_min_cppstd
+from conan.tools.files import copy
+
+
+class LogfaultConan(ConanFile):
+    name = "logfault"
+    version = "0.5.0"
+    settings = "os", "arch", "compiler", "build_type"
+    exports_sources = "include/*", "tests/*", "CMakeLists.txt"
+    no_copy_source = True
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def package(self):
+        # This will also copy the "include" folder
+        copy(self, "*.h", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        # For header-only packages, libdirs and bindirs are not used
+        # so it's necessary to set those as empty.
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+    def requirements(self):
+        self.test_requires("gtest/1.14.0")
+
+    def validate(self):
+        check_min_cppstd(self, 14)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+            cmake.test()
+
+    def package(self):
+        # This will also copy the "include" folder
+        copy(self, "*.h", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        # For header-only packages, libdirs and bindirs are not used
+        # so it's necessary to set those as empty.
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+    def package_id(self):
+        self.info.clear()
