78956696a47dde92bc1bbc1324f08137165b58cc
Jarle Aase <jarle@jgaa.com>
2024-03-17 15:53:34 +0200

    Tried to imporve the conan config a bit

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 from conan.tools.build import check_max_cppstd, check_min_cppstd
 from conan.tools.files import copy
+from conan.tools.scm import Git
 
 
 class LogfaultConan(ConanFile):
@@ -11,6 +12,12 @@ class LogfaultConan(ConanFile):
     exports_sources = "include/*", "tests/*", "CMakeLists.txt"
     no_copy_source = True
     generators = "CMakeToolchain", "CMakeDeps"
+    options = {"with_tests": [True, False]}
+    default_options = {"with_tests": False}
+
+    # def source(self):
+    #     git = Git(self)
+    #     git.clone(url="https://github.com/conan-io/libhello.git", target=".")
 
     def package(self):
         # This will also copy the "include" folder
@@ -23,7 +30,8 @@ class LogfaultConan(ConanFile):
         self.cpp_info.libdirs = []
 
     def requirements(self):
-        self.test_requires("gtest/1.14.0")
+        if not self.conf.get("tools.build:skip_test", default=False):
+            self.test_requires("gtest/1.14.0")
 
     def validate(self):
         check_min_cppstd(self, 14)
@@ -40,6 +48,7 @@ class LogfaultConan(ConanFile):
 
     def package(self):
         # This will also copy the "include" folder
+        copy(self, "LICENSE", self.source_folder, self.package_folder)
         copy(self, "*.h", self.source_folder, self.package_folder)
 
     def package_info(self):
